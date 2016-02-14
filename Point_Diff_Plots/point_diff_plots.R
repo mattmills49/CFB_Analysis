facde2b9714b1cf0aee8d67b677b98d95e3e4d8e
mattmills49 <mattmills49@gmail.com>
2016-02-14 10:09:34 -0500

    Created Point Diff Folder

---
+++ Point_Diff_Plots/point_diff_plots.R
@@ -0,0 +1,68 @@
+#' # CFB Point Differential Charts
+#'
+#+
+library(readr)
+library(stringr)
+library(ggplot2)
+library(dplyr)
+options(dplyr.width = Inf)
+source("~/Documents/CFB Data/WinProbPoster/Win_Prob_Shiny/data/AddTime.r")
+
+theme_538 <- theme(panel.background=element_rect(fill="#F0F0F0")) +
+  theme(plot.background=element_rect(fill="#F0F0F0")) +
+  theme(axis.ticks=element_blank()) +
+  theme(plot.title=element_text(face="bold",hjust=-.08,vjust=2,colour="#3C3C3C",size=20)) +
+  theme(axis.text.x=element_text(size=10,colour="#535353",face="bold")) +
+  theme(axis.text.y=element_text(size=10,colour="#535353",face="bold")) +
+  theme(axis.title.y=element_text(size=10,colour="#535353",face="bold",vjust=1.5)) +
+  theme(axis.title.x=element_text(size=10,colour="#535353",face="bold",vjust=-.5)) +
+  theme(plot.margin = unit(c(1, 1, .5, .7), "cm"))
+
+play_df <- read_csv(file = "~/Dropbox/2015week14/play.csv")
+conf_df <- read_csv(file = "~/Dropbox/2015week14/conference.csv")
+team_df <- read_csv(file = "~/Dropbox/2015week14/team.csv")
+
+names(play_df) <- str_replace_all(names(play_df), " ", "_")
+names(conf_df) <- str_replace_all(names(conf_df), " ", "_")
+names(team_df) <- str_replace_all(names(team_df), " ", "_")
+names(team_df)[2] <- "Team_Name"
+names(conf_df)[2] <- "Conf_Name"
+
+play_df <- AddTime(play_df)
+play_df <- select(play_df, -scoreTextVector, -driveTextVector)
+
+team_conf_df <- left_join(team_df, conf_df, by = c("Conference_Code"))
+
+games <- select(play_df, Game_Code, Offense_Team_Code, Defense_Team_Code) %>%
+  distinct %>%
+  rename(Team = Offense_Team_Code, Opponent = Defense_Team_Code) %>%
+  left_join(team_conf_df, by = c("Team" = "Team_Code")) %>%
+  left_join(select(team_conf_df, Team_Code, Team_Name) %>% rename(Opponent_Name = Team_Name), by = c("Opponent" = "Team_Code"))
+
+plot_df <- left_join(games, play_df, by = c("Game_Code")) %>%
+  filter(Play_Type != "KICKOFF", Period_Number <= 4) %>%
+  mutate(Point_Diff = ifelse(Team == Offense_Team_Code, Offense_Points - Defense_Points, Defense_Points - Offense_Points),
+         Time_Remaining = New.Clock + (4 - Period_Number) * 900,
+         Min_Elapsed = 60 - Time_Remaining / 60)
+
+filter(plot_df, Team_Name == "Florida State") %>%
+  ggplot(aes(x = Min_Elapsed, y = Point_Diff, group = Opponent, color = Point_Diff)) +
+  geom_line() +
+  geom_hline(yintercept = 0, linetype = "dashed") + theme(legend.position = "none") +
+  theme_538 +
+  scale_color_gradient2(low = "darkorange", high = "blue", mid = "grey85", midpoint = 0) +
+  ggtitle("FSU Point Differential by Minute") +
+  ylab("Point Differential") +
+  scale_x_continuous(name = "Game Time", breaks = seq(0, 60, by = 15))
+
+filter(plot_df, Conf_Name == "Atlantic Coast Conference") %>%
+  ggplot(aes(x = Min_Elapsed, y = Point_Diff, group = Opponent, color = Point_Diff)) +
+  geom_line() +
+  facet_wrap(~Team_Name, nrow = 3) +
+  geom_hline(yintercept = 0, linetype = "dashed") + theme(legend.position = "none") +
+  theme_538 +
+  scale_color_gradient2(low = "darkorange", high = "blue", mid = "grey85", midpoint = 0) +
+  ggtitle("ACC Point Differential by Minute") +
+  ylab("Point Differential") +
+  theme(strip.text = element_text(face = "bold", size = 12)) + 
+  scale_x_continuous(name = "Game Time", breaks = seq(0, 60, by = 15))
