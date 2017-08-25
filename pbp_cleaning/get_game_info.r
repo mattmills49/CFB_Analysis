328d00baa66cfe288a92afdc955ee08a9ea00e0d
mattmills49 <mattmills49@gmail.com>
2017-08-25 08:47:40 -0400

    updated to just use a for loop to read in and get data

--- pbp_cleaning/get_game_info.r
+++ pbp_cleaning/get_game_info.r
@@ -35,6 +35,8 @@ get_game_info <- function(json_file){
   game_info$season <- json_file[["season"]][["year"]]
   game_info$season_type <- json_file[["season"]][["type"]]
   
+  game_info$week <- with(game_info, ifelse(season_type == 3, week == 16))
+  
   return(game_info)
 }
 
@@ -44,22 +46,19 @@ save_files <- "/Users/MM/Documents/fb_analysis/pbp_cleaning/cleaned_files/%i/gam
 
 years <- 2015:2016
 
-all_files <- map2_chr(file_locations, years, sprintf) %>% 
-  map(list.files, full.names = T) %>%
-  map(~ sprintf("%s/full", .x)) %>%
-  map(function(x){
-    map(x, list.files, full.names = T) %>% flatten_chr
-  })
-
-game_files <- all_files %>%
-  map(function(x){
-    map(x, ~ fromJSON(file = .x))
-  })
-
-game_info <- game_files %>%
-  map(function(x){
-    map_dfr(x, get_game_info)
-  })
+for(y in years){
+  
+  all_files <- sprintf(file_locations, y) %>%
+    list.files(full.names = T) %>%
+    map_chr(~ sprintf("%s/full", .x)) %>%
+    map(list.files, full.names = T) %>%
+    flatten_chr
+  
+  game_files <- all_files %>%
+    map(~ fromJSON(file = .x))
+  
+  game_info <- game_files %>%
+    map_dfr(get_game_info)
 # game_id home_team away_team home_score away_score neutral_site              date
 # 1 400876038      2636       167         20         23         TRUE 2016-12-17T19:00Z
 # 2 400876039        21       248         34         10         TRUE 2016-12-17T20:30Z
@@ -68,7 +67,8 @@ game_info <- game_files %>%
 # 1    1   2016           3
 # 2    1   2016           3
 # 3    1   2016           3
-
-saved_files <- map2_chr(save_files, years, sprintf) %>%
-  map2(game_info, ~ saveRDS(.y, file = .x))
+  
+  saved_file <- sprintf(save_files, y) %>%
+    saveRDS(game_info, file = .)
+}
 
